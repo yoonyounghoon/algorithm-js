b9ccbc01d42149d8cdeb85de17e78554beac7d27
yoonyounghoon <yunyounghoon23@gmail.com>
2021-05-14 13:48:18 +0900

    수정 : Lv2.skillTree

--- Lv2/skillTree.js
+++ Lv2/skillTree.js
@@ -3,29 +3,24 @@
 
 function solution(skill, skill_trees) {
   let count = 0;
-
-  const filterdTree = skill_trees.map((value)=> {
+  
+  const filterdTree = skill_trees.map((value)=>{
     return value.split("").filter((value)=> skill.includes(value)).join("");
   });
 
-  for(let i=0; i<filterdTree.length; i++){
-    let isValid = false;
-    for(let j=0; j<filterdTree[i].length; j++){
-      if(skill[0] !== filterdTree[i][0]){
+  for(let i=0; i< filterdTree.length; i++){
+    let isValid = true;
+    for(let j=0; j < filterdTree[i].length; j++){
+      if(filterdTree[i][j] !== skill[j]){
+        isValid = false;
         break;
       }
-      isValid = true;
     }
     if(isValid){
       count += 1;
     }
   }
-  
   return count;
 }
 
 console.log(solution("CBD",["BACDE","CBADF","AECB","BDA"]));
-
-// 1. 각 배열안 문자열마다 체킹을 한다.
-// 2. C,B,D의 순서를 체킹하고 
-// 3. B,D가 C 앞에 나오는 경우.     
