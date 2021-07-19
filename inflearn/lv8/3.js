f5a11fd9ab15f71c67730704f28cc0ec0e56fb0a
yoonyounghoon <yunyounghoon23@gmail.com>
2021-07-19 14:09:30 +0900

    이진트리 순회

---
+++ inflearn/lv8/3.js
@@ -0,0 +1,17 @@
+function solution(v) {
+  let answer;
+
+  function DFS(v) {
+    if (v > 7) {
+      return;
+    } else {
+      console.log(v);
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+    }
+  }
+  DFS(v);
+  return answer;
+}
+
+console.log(solution(1));
